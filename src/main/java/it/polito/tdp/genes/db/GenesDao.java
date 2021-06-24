1f6f2abc178ad6e963b8f817b52f47ce78357e11
LilCinta <LilCinta@localhost>
2021-06-25 00:37:56 +0200

    implementazione prima query db

--- src/main/java/it/polito/tdp/genes/db/GenesDao.java
+++ src/main/java/it/polito/tdp/genes/db/GenesDao.java
@@ -37,8 +37,35 @@ public class GenesDao {
 			return null;
 		}
 	}
-	
+		
+		public List<Genes> getAllEssentialGenes(){
+			String sql = "select distinct GeneID, Essential, Chromosome \n" + 
+					"from `genes`\n" + 
+					"where Essential = 'Essential'\n" + 
+					"order by GeneId";
+			
+			List<Genes> result = new ArrayList<Genes>();
+			Connection conn = DBConnect.getConnection();
 
+			try {
+				PreparedStatement st = conn.prepareStatement(sql);
+				ResultSet res = st.executeQuery();
+				while (res.next()) {
 
+					Genes genes = new Genes(res.getString("GeneID"), 
+							res.getString("Essential"), 
+							res.getInt("Chromosome"));
+					result.add(genes);
+				}
+				res.close();
+				st.close();
+				conn.close();
+				return result;
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+	}
 	
 }
--- src/main/java/it/polito/tdp/genes/db/TestDao.java
+++ src/main/java/it/polito/tdp/genes/db/TestDao.java
@@ -9,7 +9,7 @@ public class TestDao {
 	public static void main(String[] args) {
 
 		GenesDao dao = new GenesDao();
-		List<Genes> list = dao.getAllGenes();
+		List<Genes> list = dao.getAllEssentialGenes();
 
 		for (Genes g : list) {
 			System.out.format("%-10s %-20s %1d\n", g.getGeneId(), g.getEssential(), g.getChromosome() );
