dea47a45c214de787a0997f8f80187029c6be87f
LilCinta <LilCinta@localhost>
2021-06-30 14:09:46 +0200

    second

--- src/main/java/it/polito/tdp/genes/model/Model.java
+++ src/main/java/it/polito/tdp/genes/model/Model.java
@@ -1,6 +1,19 @@
 package it.polito.tdp.genes.model;
 
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
 public class Model {
+	
+	private List<Genes> essentialGenes;
+	private Map<String, Genes> essentialGenesIdMap;
+
+	private Graph<Genes, DefaultWeightedEdge> grafo;
+
+	
 	
 	
 }
